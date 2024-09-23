9478b1fb8744a6a4ae412404eefe11a5e05d9273
AkobirToshtemirov <toshtemirovakobir8@gmail.com>
2024-09-23 09:38:27 +0500

    first commit

---
+++ src/test/java/com/akobir/wsm/WsmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akobir.wsm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
