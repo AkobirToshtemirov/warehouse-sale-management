0b2f2e69d3401821b0cb044a1fa7ce879edbafb6
AkobirToshtemirov <toshtemirovakobir8@gmail.com>
2024-09-25 12:02:23 +0500

    add auditing

--- src/main/java/com/akobir/wsm/entity/BaseEntity.java
+++ src/main/java/com/akobir/wsm/entity/BaseEntity.java
@@ -9,6 +9,7 @@ import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -18,6 +19,7 @@ import java.util.UUID;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity {
 
     @Id
